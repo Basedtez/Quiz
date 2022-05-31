87633917799ee032cd61d83607049864488df3df
Montez Burt <Griffenc4@gmail.com>
2022-05-30 22:40:56 -0500

    created question and score functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,3 +21,63 @@ class Quiz {
     }
     
 }
+// creating question class and function
+class Questions {
+    constructor(text, options, answer) {
+        this.text = text;
+        this.options = options;
+        this.answer = answer;
+    }
+    isCorrectAnswer(choice) {
+        return this.answer === choice;
+    }
+}
+
+function displayQuestions() {
+    if (quiz.isEnded()) {
+        showScores();
+    }else {
+        
+        let questionElement =document.getElementById("questions");
+        questionElement.innerHTML = quiz.getQuestionIndex().text;
+        let options = quiz.getQuestionIndex().options;
+        for (let i = 0; i < options.length; i++) {
+            let optionsElement = document.getElementById("option" + i);
+            optionsElement.innerHTML = options[i];
+            selected("btn" + i, options[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function selected(id, selected) {
+    let button = document.getElementById(id);
+    button.onclick = function(); {
+        quiz.selected(selected);
+        displayQuestions();
+    }
+    
+}
+
+function showProgress() {
+    let currentQuestionNumber = quiz.questionIndex + 1;
+    let progressElement = document.getElementById("quiz-prog")
+    progressElement.innerHTML = `Question ${currentQuestionNumber} of ${quiz.Questions.length} `;
+}
+
+// question score count
+function showScores() {
+    let quizEndHTML =
+    `
+        <h1>Quiz completed</h1>
+        <h2 id="score">Your results: ${quiz.score} of ${quiz.question.length} </h2>
+        <div class="quiz-repeat">
+            <a href="index.html">Take Quiz Again</a>
+        
+        </div>
+
+    `;
+    let quizElement = document.getElementById("quiz");
+    quizElement.innerHTML = quizEndHTML;    
+}
